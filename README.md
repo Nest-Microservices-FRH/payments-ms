6e673a99f8542a31565a85c0c5dbe39289104ff9
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-13 17:53:20 +0100

    config payment session

--- README.md
+++ README.md
@@ -14,4 +14,23 @@ private readonly stripe = new Stripe(envs.stripeSecret);
 ```
 
 ## configurar payment session
+lo preparamos para recibir datos.
 
+ejemplo de postman:
+```
+{
+    "currency": "eur",
+    "items": [
+        {
+            "name": "Teclado Mecanico",
+            "price": 100,
+            "quantity": 2
+        },
+        {
+            "name": "Raton Mecanico",
+            "price": 30.99,
+            "quantity": 2
+        }
+    ]
+}
+```
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100), // 20.00 stripe no detecta el punto decimal
+        },
+        quantity: item.quantity,
+      };
+    });
+
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
         metadata: {},
       },
-      line_items: [
-        {
-          price_data: {
-            currency: 'eur',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000, // 20.00 stripe no detecta el punto decimal
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3003/payments/success',
       cancel_url: 'http://localhost:3003/payments/cancel',
